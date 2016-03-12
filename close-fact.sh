772a30fd8d259c562763f94485cc96a3c14686c8
Kevin Lyda <kevin@ie.suberic.net>
2016-03-12 12:37:00 +0000

    Fix shell output.
    sqrt() in bc will round down. So just increment it by one.

--- close-fact.sh
+++ close-fact.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 for n in $*; do
-  i=$(bc <<< "sqrt($n)")
+  i=$(bc <<< "sqrt($n) + 1")
   while [[ $i > 0 ]]; do
     if [[ $(($n % $i)) == 0 ]]; then
       echo "$n closest factors: $i x $(($n / $i))"
